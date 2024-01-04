6ab5e69f857da5f2f71272356ed2f4f1bc82e407
Erick Daniel Perez Mata <erickdanielperezmata@outlook.es>
2024-01-03 23:00:48 -0600

    create path aliases

--- src/views/main/main.tsx
+++ src/views/main/main.tsx
@@ -1,9 +1,12 @@
 import { Text, View } from "react-native";
 
+import { Form } from "@src/components";
+
 export function Main() {
   return (
     <View>
       <Text>Main</Text>
+      <Form />
     </View>
   );
 }
