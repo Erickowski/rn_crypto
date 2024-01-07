ab27cef06b0af4a4a6a68f6135d8f61ede202760
Erick Daniel Perez Mata <erickdanielperezmata@outlook.es>
2024-01-07 16:52:57 -0600

    add picker for money labels

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -1,4 +1,7 @@
 import { Text, View } from "react-native";
+import { Picker } from "@react-native-picker/picker";
+
+import { MONEY_LABELS } from "@src/types";
 
 import styles from "./form.module.scss";
 
@@ -6,6 +9,15 @@ export function Form() {
   return (
     <View style={styles.form}>
       <Text style={styles.label}>Moneda</Text>
+      <Picker>
+        {MONEY_LABELS.map((label) => (
+          <Picker.Item
+            key={label.value}
+            label={label.label}
+            value={label.value}
+          />
+        ))}
+      </Picker>
       <Text style={styles.label}>Criptomoneda</Text>
     </View>
   );
---
+++ src/types/picker/picker.ts
@@ -0,0 +1,7 @@
+export const MONEY_LABELS = [
+  { label: "- Seleccione -", value: "" },
+  { label: "Dolar de Estados Unidos", value: "USD" },
+  { label: "Peso Mexicano", value: "MXN" },
+  { label: "Euro", value: "EUR" },
+  { label: "Libra esterlina", value: "GBP" },
+];
