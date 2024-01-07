d0c1580a10d73ce0abc0ea7c7305c0d3d7d7af9c
Erick Daniel Perez Mata <erickdanielperezmata@outlook.es>
2024-01-07 15:31:13 -0600

    configure fonts

--- src/views/main/main.tsx
+++ src/views/main/main.tsx
@@ -1,10 +1,29 @@
-import { Text, View } from "react-native";
+import { useCallback } from "react";
+import { View } from "react-native";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 import { Header } from "@src/components";
 
 export function Main() {
+  const [fontsLoaded] = useFonts({
+    "Lato-Black": require("@src/assets/fonts/Lato-Black.ttf"),
+    "Lato-Regular": require("@src/assets/fonts/Lato-Regular.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
-    <View>
+    <View onLayout={onLayoutRootView}>
       <Header />
     </View>
   );
